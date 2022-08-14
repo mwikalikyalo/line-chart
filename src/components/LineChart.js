4c18aa19a67de57878024c4d6891b78ee5c1d3cb
mwikalikyalo <winniemwikali07@gmail.com>
2022-08-14 23:00:56 +0300

    Register category scale and linear scale

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -12,7 +12,15 @@ import {
 
 import { Line } from 'react-chartjs-2';
 
-
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 
 const LineChart = () => {
