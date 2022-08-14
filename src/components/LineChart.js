c093e5b786bb480c547a6bbe20646432d14a8be6
mwikalikyalo <winniemwikali07@gmail.com>
2022-08-14 22:59:50 +0300

    Import linear and category scale to the file from chart.js

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -12,15 +12,7 @@ import {
 
 import { Line } from 'react-chartjs-2';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
+
 
 
 const LineChart = () => {
